854ea2764cc7648b54b85c933af783e2b20ff561
MiZuii <piotr.wiercigroch1@gmail.com>
2024-12-02 14:26:21 +0100

    day2 done

--- day02/puzzle2.cpp
+++ day02/puzzle2.cpp
@@ -4,9 +4,48 @@
 #include <string>
 #include <filesystem>
 
+bool safe_check(std::vector<int> &levels)
+{
+    if(levels[0] == levels[1]) return false; // does not differ by at least one start case
+
+    int increasing_coef = levels[1] - levels[0] > 0 ? 1 : -1;
+    bool safe_flag = true;
+    for( auto it=levels.begin()+1; it != levels.end(); it++)
+    {
+        auto v = (*it - *(it-1))*increasing_coef;
+        if( v < 1 || v > 3 )
+        {
+            safe_flag = false;
+            break;
+        }
+    }
+
+    return safe_flag;
+}
+
 int solution(std::vector<std::string> &input)
 {
-    return 0;
+    int safe = 0;
+    for( auto line : input )
+    {
+        std::stringstream lines(line);
+        std::string token;
+        std::vector<int> levels;
+
+        while(std::getline(lines, token, ' '))
+        {
+            levels.push_back(std::stoi(token));
+        }
+
+        for(size_t i=0; i<levels.size(); i++)
+        {
+            std::vector<int> vcp(levels.begin(), levels.end());
+            vcp.erase(vcp.begin()+i);
+            if( safe_check(vcp) ) {safe++; break;}
+        }
+    }
+
+    return safe;
 }
 
 int main(int argc, char* argv[]) {
